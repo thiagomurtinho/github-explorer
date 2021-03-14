ceeec70f34712b5124ba403cc438f32788d719d4
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-03-13 21:28:36 -0300

    feat: :sparkles: Styling dashboard
    Inserting logo,  form creating and styling, list creand and styling. Installing and using polished and react-icons packages.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,80 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer Logo" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="https://">
+          <img
+            src="https://avatars.githubusercontent.com/u/12580485?s=460&u=cd3ae5a76b8402fd45c4d29f8bd837e48865d928&v=4"
+            alt="Foto de Perfil"
+          />
+          <div>
+            <strong>Título do repo</strong>
+            <p>Descrição do repo no Github</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="https://">
+          <img
+            src="https://avatars.githubusercontent.com/u/12580485?s=460&u=cd3ae5a76b8402fd45c4d29f8bd837e48865d928&v=4"
+            alt="Foto de Perfil"
+          />
+          <div>
+            <strong>Título do repo</strong>
+            <p>Descrição do repo no Github</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="https://">
+          <img
+            src="https://avatars.githubusercontent.com/u/12580485?s=460&u=cd3ae5a76b8402fd45c4d29f8bd837e48865d928&v=4"
+            alt="Foto de Perfil"
+          />
+          <div>
+            <strong>Título do repo</strong>
+            <p>Descrição do repo no Github</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="https://">
+          <img
+            src="https://avatars.githubusercontent.com/u/12580485?s=460&u=cd3ae5a76b8402fd45c4d29f8bd837e48865d928&v=4"
+            alt="Foto de Perfil"
+          />
+          <div>
+            <strong>Título do repo</strong>
+            <p>Descrição do repo no Github</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="https://">
+          <img
+            src="https://avatars.githubusercontent.com/u/12580485?s=460&u=cd3ae5a76b8402fd45c4d29f8bd837e48865d928&v=4"
+            alt="Foto de Perfil"
+          />
+          <div>
+            <strong>Título do repo</strong>
+            <p>Descrição do repo no Github</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
