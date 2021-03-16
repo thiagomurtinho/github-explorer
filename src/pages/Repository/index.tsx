48f4ef528eee5ffae4207162eeb8d173a0ad61c1
Thiago Murtinho <thiago.murtinho@poli.ufrj.br>
2021-03-15 22:02:04 -0300

    feat: :sparkles: Navigating between routes
    RepositoryInfo component creating, Issues component creating and use routesMatch() to receive params of URLs.

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +12,54 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/69590972?s=200&v=4"
+            alt=""
+          />
+          <div>
+            <strong>Título do repositório</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>123</strong>
+            <span>Start</span>
+          </li>
+          <li>
+            <strong>123</strong>
+            <span>Fork</span>
+          </li>
+          <li>
+            <strong>123</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="none">
+          <div>
+            <strong>asdasd</strong>
+            <p>asdads</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
